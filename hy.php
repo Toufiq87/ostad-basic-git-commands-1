25294efe4f60cacef71959a94e107189454ee608
Toufiq Elahi1 <toufiqelahi@gmail.com>
2023-02-02 13:06:45 -0600

    push existing project to Gitlab

--- hy.php
+++ hy.php
@@ -1,4 +1,36 @@
 <?php
+$name = "Bangladesh";
+$age = 16;
+$question = "How are you";
+$word = "age";
+
+//echo $$word;
+
+echo 'Hello'.$name;
+echo"\n";
+
+echo "Hello {$name},{$question}";
+
+
+
+
+
+
+
+
+
+
+/*
 $name1 = "toufiq";
 $name2 = "elahi";
 echo $name1, $name2;
+
+
+$name = "bangladesh";
+$age = 123;
+$myname = "Hasin hyder";
+$my_name = "";
+$myName="";
+$myBeautifullvariable = "";
+*/
+
